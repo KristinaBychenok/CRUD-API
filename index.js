419a94e06c42ecbff18de0c8611b6f5d608f0bd9
Krystsina Bychanok <Krystsina_Bychanok@epam.com>
2023-07-04 21:55:51 +0200

    feat: add all endpoints (get, post, put, delete) and error handles; add PORT variable from env; add nodemon mode

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 import http from "http";
-import { errorHandle } from "./src/errorHandle.js";
-import { getUsers, getUserById } from "./src/endpoints.js";
+import { error404Handle } from "./src/utils/errorHandle.js";
+import {
+  getUsers,
+  getUserById,
+  postUser,
+  updateUser,
+  deleteUser,
+} from "./src/endpoints.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   const routArray = req.url.split("/").filter((item) => !!item);
-
-  if (routArray.length === 3 && req.method === "GET") {
-    getUserById(req, res);
-  } else if (routArray.length === 2 && req.method === "GET") {
-    getUsers(req, res);
+  if (routArray.length === 3) {
+    switch (req.method) {
+      case "GET":
+        getUserById(req, res);
+        break;
+      case "PUT":
+        updateUser(req, res);
+        break;
+      case "DELETE":
+        deleteUser(req, res);
+        break;
+    }
+  } else if (routArray.length === 2) {
+    switch (req.method) {
+      case "GET":
+        getUsers(req, res);
+        break;
+      case "POST":
+        postUser(req, res);
+        break;
+    }
   } else {
-    errorHandle(req, res);
+    error404Handle(req, res, "Sorry, we can't find this page!");
   }
 });
 
--- src/errorHandle.js
+++
@@ -1,5 +0,0 @@
-export const errorHandle = (_req, res) => {
-  res.statusCode = 404;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Sorry, we can't find this page!");
-};
